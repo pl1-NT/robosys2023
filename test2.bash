11b6f6843465c96f3c2841cdcac972e654cc5f08
Nishi Touki <touki2424@gmail.com>
2023-11-29 16:58:06 +0900

    change the output

--- test2.bash
+++ test2.bash
@@ -7,17 +7,17 @@ res=0
 out=$(seq 5 | ./plus-times)
 onum=$(echo $out | sed -e 's/[^0-9]//g')
 ### answer confirm ###
-[ "$?" = 0 ]          || (echo end status error && er ${LINENO}) 
-[ "$onum" = "15120" ] || (echo calculate error && er ${LINENO})
+[ "$?" = 0 ]             || (echo end status error && er ${LINENO}) 
+[ "$onum" = "151203.0" ] || (echo calculate error && er ${LINENO})
 
 ### input check ###
 out=$(echo http:// | ./plus-times)
 [ "$?" = 1 ]       || (echo end status error && er ${LINENO})
-[ "${out}" = "" ] || (echo invalid output  && er ${LINENO})
+[ "${out}" = "" ]  || (echo invalid output  && er ${LINENO})
 
 out=$(echo  | ./plus-times)
 [ "$?" = 1 ]       || (echo end status error && er ${LINENO})
-[ "${out}" = "" ] || (echo invalid output  && er ${LINENO})
+[ "${out}" = "" ]  || (echo invalid output  && er ${LINENO})
 
 [ "$res" = 0 ] && echo all correct
 exit $res
