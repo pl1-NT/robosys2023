869357a121e6d8542be95d9d9096a135d50c4787
Nishi Touki <touki2424@gmail.com>
2023-11-29 10:05:18 +0900

    add contents

--- test.bash
+++ test.bash
@@ -1,5 +1,15 @@
-#!/bin/bash -xv
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2023 Touki Nishi
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng(){
+	echo error at Line $1
+	res=1
+}
+res=0
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "$out" = 15 ]
+[ "$out" = 15 ] || ng $LINENO
+[ "$res" = 0 ] && echo OK
+exit $res
