8ac5db4394844bc543bed2db0338328df6d3a6e8
Nishi Touki <touki2424@gmail.com>
2023-11-29 16:02:11 +0900

    expand test contents

--- test2.bash
+++ test2.bash
@@ -1,9 +1,24 @@
 #!/bin/bash
+er(){
+	echo at LINE $1
+	res=1	
+}
 res=0
-out=$(seq 5 | ./plus)
+out=$(seq 5 | ./plus-times)
 onum=$(echo $out | sed -e 's/[^0-9]//g')
+### answer confirm ###
+[ "$?" = 0 ]          || (echo end status error && er ${LINENO}) 
+[ "$onum" = "15120" ] || (echo calculate error && er ${LINENO})
 
-[ "$onum" = "15120" ] || echo 不一致 
-[ "$res" = 0 ] && echo OK
+### input check ###
+out=$(echo http:// | ./plus-times)
+[ "$?" = 1 ]       || (echo end status error && er ${LINENO})
+[ "${out}" = "" ] || (echo invalid output  && er ${LINENO})
+
+out=$(echo  | ./plus-times)
+[ "$?" = 1 ]       || (echo end status error && er ${LINENO})
+[ "${out}" = "" ] || (echo invalid output  && er ${LINENO})
+
+[ "$res" = 0 ] && echo all correct
 exit $res
 
